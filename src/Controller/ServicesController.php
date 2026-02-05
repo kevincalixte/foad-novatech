efd181c879d3bf393e08642e63c6bc905725a10b
Kevin Calixte <kevincalixte@MacBook-Pro-de-Kevin.local>
2026-02-05 23:02:13 +0100

    foad 06-02

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -2,20 +2,94 @@
 
 namespace App\Controller;
 
+use App\Entity\Service;
+use App\Form\ServiceType;
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ServicesController extends AbstractController
 {
+    // FOAD 04-02 
+    // #[Route('/services', name: 'services')]
+    // public function index(): Response
+    // {
+    //     $page = ["title" => "NovaTech - Services", "subject" => "Our Services"];
+    //     $services = ["Web Development","Technical Consulting","Code Audit","Application Maintenance"];
+    //     return $this->render('services/index.html.twig', [
+    //         'page' => $page,
+    //         'services' => $services,
+    //     ]);
+    // }
+
+    // FOAD 06-02
     #[Route('/services', name: 'services')]
-    public function index(): Response
+    public function index(ServiceRepository $servicesRepository): Response
     {
-        $page = ["title" => "NovaTech - Services", "subject" => "Our Services"];
-        $services = ["Web Development","Technical Consulting","Code Audit","Application Maintenance"];
+        $services = $servicesRepository->findAll();
+        // dd($services);
         return $this->render('services/index.html.twig', [
-            'page' => $page,
             'services' => $services,
         ]);
+    }
+
+    //CEREATE
+    #[Route('/services/new', name: 'new_event')]
+    public function new(Request $request, EntityManagerInterface $em)
+    {
+        $services = new Service();
+        $form = $this->createForm(ServiceType::class, $services);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $services->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($services);
+            $em->flush();
+            return $this->redirectToRoute("services");
+        }
+        return $this->render("services/new.html.twig", [
+            "formulaire" => $form,
+        ]);
+    }
+
+    //READ
+    #[Route('/services/{id}', name: 'show_service')]
+    public function show(Service $services)
+    {
+        return $this->render("services/show.html.twig", [
+            "services" => $services,
+        ]);
+    }
+
+    //UPDATE
+    #[Route('/services/edit/{id}', name: 'edit_service')]
+    public function edit(Request $request, EntityManagerInterface $em, Service $services)
+    {
+
+        $form = $this->createForm(ServiceType::class, $services);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $services->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($services);
+            $em->flush();
+            return $this->redirectToRoute("services");
+        }
+        return $this->render("services/edit.html.twig", [
+            "formulaire" => $form,
+            "services" => $services,
+        ]);
+    }
+
+    //DELETE
+    #[Route('/services/{id}', name: 'delete_service', methods: ["POST"])]
+    public function delete(Request $request,Service $service, EntityManagerInterface $em): Response
+    {
+         if ($this->isCsrfTokenValid('delete'.$service->getId(), $request->request->get('_token'))) {
+        $em->remove($service);
+        $em->flush();
+         }
+        return $this->redirectToRoute('services');
     }
 }
---
+++ src/Entity/Service.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServiceRepository::class)]
+class Service
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?bool $isActive = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): static
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
